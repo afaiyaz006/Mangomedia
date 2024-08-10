e121d5bcae16dcf8ea7c484b8bc2162d5fdbc947
Ahmed Faiyaz <69722755+afaiyaz006@users.noreply.github.com>
2024-08-11 02:26:27 +0600

    updated config

--- Mangomedia_Project/settings.py
+++ Mangomedia_Project/settings.py
@@ -136,8 +136,8 @@ DATABASES = {
     'PASSWORD': getenv('PGPASSWORD'),
     'HOST': getenv('PGHOST'),
     'PORT': getenv('PGPORT', 5432),
-    'OPTIONS': {
-      'sslmode': 'require',
-    },
+   # 'OPTIONS': {
+   #   'sslmode': 'require',
+   # },
   }
 }
