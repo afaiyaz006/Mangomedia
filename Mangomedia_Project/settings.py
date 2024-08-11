fa62319f9bd2807b0f4174c895723dca4bb96d87
Ahmed Faiyaz <69722755+afaiyaz006@users.noreply.github.com>
2024-08-11 20:21:55 +0600

    secret removed

--- Mangomedia_Project/settings.py
+++ Mangomedia_Project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-2h5krivzemd20##2p)@ojrhpo616@gt!!^o$%lducuvrt(^h$q"
+SECRET_KEY = getenv('SECRET_KEY') # CHECKED NOT USING default ones.
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
