ca92381914dae6218c65ea473fd91f7a96856561
Ahmed Faiyaz <69722755+afaiyaz006@users.noreply.github.com>
2024-08-13 02:49:19 +0600

    generated static files

--- Mangomedia_Project/settings.py
+++ Mangomedia_Project/settings.py
@@ -154,7 +154,7 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DATABASES = {
